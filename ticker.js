746486814a4e6ede67db9759b4a2e60302806a92
moseralexj <moseralexj@gmail.com>
2026-03-09 08:57:44 -0700

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -1,7 +1,6 @@
 // ticker.js
 window.ABCOAST_TICKER = [
-  "No major delays.",
-  "Rain expected throughout the week.",
+  "Snow expected throughout the week.",
   "Remember to wear high-visibility vests on the ramp.",
   "Welcome to ABCOAST Aviation Services."
 ];
