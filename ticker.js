a43a115b132e809064decf3900706993ad5ccaa5
moseralexj <moseralexj@gmail.com>
2026-03-16 05:09:48 +0800

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -1,6 +1,6 @@
 // ticker.js
 window.ABCOAST_TICKER = [
-  "Snow expected throughout the week.",
+  "Alex in MNL until March 31.",
   "Remember to wear high-visibility vests on the ramp.",
   "Welcome to ABCOAST Aviation Services."
 ];
